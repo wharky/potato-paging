83b924e3c8db3f19e3a61a3939badf94f3fb197f
wharky <wharkpark@gmail.com>
2023-05-25 17:31:11 +0900

    Initial commit

---
+++ src/main/java/com/example/potatopaging/PotatoPagingApplication.java
@@ -0,0 +1,13 @@
+package com.example.potatopaging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PotatoPagingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PotatoPagingApplication.class, args);
+    }
+
+}
